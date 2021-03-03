c843ec12be3c27275809450664982beffcda69bf
Marc Munar <marc.munar@uib.es>
2021-03-03 16:30:15 +0100

    Create E1.m

---
+++ Taller0/E1.m
@@ -0,0 +1,22 @@
+%EJERCICIO 1: Resolver la ecuación matricial 2X+4A=3*B*A.
+
+A = [0, -1; 2, 1];
+B = [1, 2;3, 4];
+
+% Si tenemos la ecuación 2X+4A=3*B*A, un cálculo sencillo nos muestra
+% que 2X = 3*B*A-4*A, por lo que X = 1/2*(3*B*A-4*A)
+% que X = 1/2*(3*B*A-4*A). Por lo tanto:
+
+X = 0.5*(3*B*A-4*A);
+disp("Matriz solución")
+disp(X)
+
+% Podemos comprobar que es efectuvamente solución
+left_matrix = 2*X+4*A;
+right_matrix = 3*B*A;
+
+disp("Matriz del lado izquierdo de la igualdad")
+disp(left_matrix)
+
+disp("Matriz del lado derecho de la igualdad")
+disp(right_matrix)

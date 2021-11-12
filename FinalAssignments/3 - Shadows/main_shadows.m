ec817054a7b6ca6d449565d4b2b45a475f69f0e4
Marc Munar <marc.munar@uib.es>
2021-11-12 10:59:43 +0100

    Create main_shadows.m

---
+++ FinalAssignments/3 - Shadows/main_shadows.m
@@ -0,0 +1,13 @@
+%%%%%%%% PARÁMETROS DEL PROBLEMA %%%%%%%%
+
+%domain1 = linspace(i1, i2, K);
+%domain2 = linspace(j1, j2, M);
+%sun_point = [s1, s2, s3];
+%plane_coefficients = [a, b, c, d];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%surface_points = generate_surface(domain1, domain2);
+%shadows = generate_shadows(curve_points, sun_point, plane_coefficients);
+
+%plot_scene(surface_points, sun_point, shadows, plane_coefficients);

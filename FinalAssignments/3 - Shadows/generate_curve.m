56bea9cd2f162cee4ab070fba43acf109e861480
Marc Munar <marc.munar@uib.es>
2021-11-12 11:00:53 +0100

    Create generate_curve.m

---
+++ FinalAssignments/3 - Shadows/generate_curve.m
@@ -0,0 +1,12 @@
+function H = generate_curve(domain)
+  x = [];
+  y = [];
+  z = [];
+  
+  for t = domain
+    x = [x ## COMPLETAR ##];
+    y = [y ## COMPLETAR ##];
+    z = [z ## COMPLETAR ##];
+  endfor
+  H = [x; y; z]';
+endfunction

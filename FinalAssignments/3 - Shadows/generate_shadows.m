3abffba8800a31b9b4f686f98e5e5f204571a97a
Marc Munar <marc.munar@uib.es>
2021-11-12 11:01:13 +0100

    Create generate_shadows.m

---
+++ FinalAssignments/3 - Shadows/generate_shadows.m
@@ -0,0 +1,6 @@
+function shadows = generate_shadows(surface_points, sun_point, plane_coefficients)
+  [n_rows, n_columns] = size(surface_points);
+  shadows = zeros(n_rows, n_columns);
+  
+  ## COMPLETAR ##
+endfunction
